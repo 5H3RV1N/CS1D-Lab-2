c5666efc2b1a679c1bf1720b4a19006621abd25f
ParsaKhazravi <46808147+ParsaKhazravi@users.noreply.github.com>
2020-01-15 14:15:28 -0800

    Add files via upload

---
+++ hello2Function.cpp
@@ -0,0 +1,11 @@
+/*
+ * hello2Function.cpp
+ * PULL REQUEST
+ */
+#include <iostream>
+using namespace std;
+int hello2Function(string name2)
+{
+	cout << "hello World " << name2 << endl;
+	return 0;
+}
